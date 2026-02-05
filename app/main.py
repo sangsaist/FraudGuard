1b1b94c971b0c25eda5ce1ff401f79d7156629c1
sangsaist <sanjaysangsai@gmail.com>
2026-02-05 21:16:20 +0530

    Fix

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, Request, HTTPException
 from fastapi.responses import JSONResponse
 
-from config import API_KEY
+from .config import API_KEY
 from orchestrator.orchestrator import handle_request
 
 app = FastAPI()
